23864d1418d6b55f84d93fd1635910653b1c8303
En Matos <108830642+ematosrj@users.noreply.github.com>
2023-02-13 09:55:24 -0300

    cadastro competidores php

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CADASTRO DE COMPETIDORES</title>
+</head>
+<body>
+<p><b>FORMULÁRIO PARA INSCRIÇÃO DE COMPETIDORES</b></p>    
+<form action="script.php" method="post">
+<p>Seu Nome :<input type="text" name="nome"></p>
+<p>Sua Idade :<input type="text" name="idade"></p>
+<p><input type="submit"></p>
+</form>    
+</body>
+</html>
---
+++ script.php
@@ -0,0 +1,18 @@
+<?php
+
+$categorias = [];
+$categorias[] = 'infantil';
+$categorias[] = 'adolescente';
+$categorias[] = 'adulto';
+$categorias[] = 'idoso';
+
+$nome = $_POST [] ;
+$idade = $_POST [] ;
+
+
+
+
+var_dump($nome);
+var_dump($idade);
+
+?>
